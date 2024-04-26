2d9b039fcb27a9915c8faedb9d511e933a8fc1b3
jalwana <mohammad.jalwana@fortescue.com>
2024-04-26 10:33:51 +0800

    :construction: included code for pytest

---
+++ tests/test_linear_algebra.py
@@ -0,0 +1,13 @@
+import numpy as np
+from LinearAlgebra.la import find_inverse
+
+
+def test_invalid_find_inverse():
+    assert find_inverse(None) is None
+
+
+def test_valid_find_inverse():
+    matrix = [[1, 2], [3, 4]]
+    expected = [[-2., 1.],
+                [1.5, -0.5]]
+    assert np.isclose(find_inverse(matrix), expected, atol=0.01).all()
